f0edcd26ef81681fe5c63c9f0711da82f4910126
StefanoArcaro <stefanoarc00@gmail.com>
2022-12-01 01:12:33 +0100

    feat(SDL2): integrated basic window

--- Makefile
+++ Makefile
@@ -1,9 +1,23 @@
+# CC specifies which compiler we're using
 CC=g++
+
+# CFLAGS specifies all the additional compilation options
 CFLAGS= -std=c++17
+
+# LFLAGS specifies the libraries we're linking against
+LFLAGS = -lSDL2
+
+# SRC specifies which files to compile
 SRC=./src/*.cpp
+
+# INCLUDE specifies the root of the include path
 INCLUDE=./include
+
+# EXE specifies the name of our executable
 EXE=app
+
+# TODO specify
 LIB_OSX=./lib/osx
 
 osx:
-	$(CC) $(CFLAGS) $(SRC) -I$(INCLUDE) -o $(EXE) -L$(LIB_OSX)
+	$(CC) $(SRC) $(CFLAGS) $(LFLAGS) -I$(INCLUDE) -o $(EXE) -L$(LIB_OSX)
--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,50 @@
 #include <cmath>
 #include <string>
 
-int main(int argc, char **argv) {
+//Using SDL and standard IO
+#include <SDL2/SDL.h>
+#include <stdio.h>
+
+//Screen dimension constants
+const int SCREEN_WIDTH = 1920;
+const int SCREEN_HEIGHT = 1080;
+
+int main( int argc, char* args[] )
+{
+	//The window we'll be rendering to
+	SDL_Window* window = NULL;
+	
+	//The surface contained by the window
+	SDL_Surface* screenSurface = NULL;
+
+	//Initialize SDL
+	SDL_Init(SDL_INIT_EVERYTHING);
+
+    //Create window
+    window = SDL_CreateWindow("Tower Defense", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+		
+    //Get window surface
+    screenSurface = SDL_GetWindowSurface(window);
+
+    //Fill the surface white
+    SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF ));
+    
+    //Update the surface
+    SDL_UpdateWindowSurface(window);
+    
+    //Hack to get window to stay up
+    SDL_Event e;
+    bool quit = false;
+    while(quit == false) { while(SDL_PollEvent(&e)) { if(e.type == SDL_QUIT) quit = true; } }
+
+	//Destroy window
+	SDL_DestroyWindow(window);
+
+	//Quit SDL subsystems
+	SDL_Quit();
+
+
+    // Test
     auto a = Vec2(1, 2);
     auto b = Vec2(3, 4);
     auto c = Vec2(5, 6);
@@ -18,7 +61,7 @@ int main(int argc, char **argv) {
 
     std::cout << std::string(a) << '\t' << std::string(tr.pos) << std::endl;
 
-    return 0;
+	return 0;
 }
 
 /*
